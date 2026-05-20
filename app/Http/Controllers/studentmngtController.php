9f3f94e3d56b7b5d834a7c25284dc1203e382cbb
JustinReianneAntonio <justinreianneantonio29@gmail.com>
2026-05-20 14:12:44 +0800

    Models and table

--- app/Http/Controllers/studentmngtController.php
+++ app/Http/Controllers/studentmngtController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App/Models/student;
 class studentmngtController extends Controller
 {
+
     public function index () {
+        $students = student:: all();
         return view ('student.index');
     }
 
